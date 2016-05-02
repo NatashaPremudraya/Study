c9132e2b7ee833b65d4c83b5f908864a2efad2b5
natashapremudraya <tusya_03_01@mail.ru>
2016-05-02 12:58:52 +0400

    запилила Timer с IDisposable

---
+++ MemoryManagement/Timer/Timer/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Timer
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TistTimer()
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+
+            using (timer.Continue())
+            {
+                Thread.Sleep(500);
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ MemoryManagement/Timer/Timer/Timer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    public class Timer
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        public long ElapsedMilliseconds => stopwatch.ElapsedMilliseconds;
+
+        public TimerToken Start()
+        {
+            stopwatch.Start();
+            return new TimerToken(this);
+        }
+
+        private void Stop()
+        {
+            stopwatch.Stop();
+        }
+
+        public TimerToken Continue()
+        {
+            stopwatch.Start();
+            return new TimerToken(this);
+        }
+
+        public class TimerToken : IDisposable
+        {
+            private Timer timer;
+            public TimerToken(Timer timer)
+            {
+                this.timer = timer;
+            }
+
+            public void Dispose()
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
