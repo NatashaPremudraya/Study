1b79a62a7aae3fec00dcab272b515b58abaf721e
natashapremudraya <tusya_03_01@mail.ru>
2016-05-26 15:54:36 +0400

    читаем csv-файл, и возвращаем ленивое перечисление объектов string[]

---
+++ Files/Files/Files/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    class Program
+    {
+        public static IEnumerable<string> ReadCSV1(string filename)
+        {
+            using (var stream = new StreamReader(filename))
+            {
+                while (true)
+                {
+                    var line = stream.ReadLine();
+                    if (line == null) break;
+                    yield return line;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Files/Files/Files/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Files
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestReadCSV1()
+        {
+            var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
+            var lines = Program.ReadCSV1(filename);
+            Assert.AreEqual("\"Sensor3\",12,149,12.6,74,5,3", lines.Take(4).Last());
+        }
+    }
+}
