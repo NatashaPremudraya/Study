fe88e0de67525f52fd3dd6f936260ee239171876
natashapremudraya <tusya_03_01@mail.ru>
2016-05-26 18:47:20 +0400

    Этот говнокод для второго задания работает, но просит беспощадного рефакторинга

--- Files/Files/Files/Program.cs
+++ Files/Files/Files/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using Microsoft.VisualBasic.FileIO;    
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Files
 {
+    public class AirQualityItem
+    {
+        public string Name { get; set; }
+        public int? SolarR { get; set; }
+        public int? Ozone { get; set; }
+        public double Wind { get; set; }
+        public int Temp { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+    }
+
     class Program
     {
         public static IEnumerable<string> ReadCSV1(string filename)
@@ -24,6 +39,89 @@ namespace Files
 
         static void Main(string[] args)
         {
+        }
+
+        public static IEnumerable<T> ReadCSV2<T>(string filename)
+        {
+            var lines = ReadCSV1(filename);
+            var fieldNames = lines
+                .First()
+                .Split(',')
+                .Select(x => x.Substring(1, x.Length - 2))
+                .Select(x => x.Replace(".",""))
+                .ToArray();
+            foreach (var line in lines.Skip(1))
+            {
+                var lineValues = line.Split(',');
+                var resultItemConstructor = typeof (T).GetConstructor(new Type[] {});
+                var resultItem = resultItemConstructor.Invoke(new object[] {});
+                for (var i = 0; i < fieldNames.Length; i++)
+                {
+                    var type = typeof (T).GetProperty(fieldNames[i]).PropertyType;
+                    if (type == typeof (int))
+                    {
+                        try
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, int.Parse(lineValues[i]));
+                        }
+                        catch (Exception)
+                        {
+                            throw new FormatException();
+                        }
+                        continue;
+                    }
+                    if (type == typeof (int?))
+                    {
+                        try
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, int.Parse(lineValues[i]));
+                        }
+                        catch (Exception)
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, null);
+                        }
+                        continue;
+                    }
+                    if (type == typeof (double))
+                    {
+                        try
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, double.Parse(lineValues[i], new CultureInfo("en-us")));
+                        }
+                        catch (Exception)
+                        {
+                            throw new FormatException();
+                        }
+                        continue;
+                    }
+                    if (type == typeof(double?))
+                    {
+                        try
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, double.Parse(lineValues[i]));
+                        }
+                        catch (Exception)
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, null);
+                        }
+                        continue;
+                    }
+                    if (type == typeof (string))
+                    {
+                        try
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, lineValues[i].Substring(1, lineValues[i].Length-2));
+                        }
+                        catch (Exception)
+                        {
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, null);
+                        }
+                        continue;
+                    }
+
+                }
+                yield return (T) resultItem;
+            }
         }
     }
 }
--- Files/Files/Files/Tests.cs
+++ Files/Files/Files/Tests.cs
@@ -7,11 +7,23 @@ namespace Files
     public class Tests
     {
         [Test]
-        public void TestReadCSV1()
+        public void TestFirstReadCSV()
         {
             var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
             var lines = Program.ReadCSV1(filename);
             Assert.AreEqual("\"Sensor3\",12,149,12.6,74,5,3", lines.Take(4).Last());
         }
+
+        [Test]
+        public void TestSecondReadCSV2()
+        {
+            var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
+            var objs = Program.ReadCSV2<AirQualityItem>(filename);
+            var sensor1 = objs.Take(1).ToArray()[0];
+            Assert.AreEqual("Sensor1",sensor1.Name);
+            Assert.AreEqual(41, sensor1.Ozone);
+        }
+
+
     }
 }
