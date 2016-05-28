fd6ce81052573ab25e581654222461182a384f48
natashapremudraya <tusya_03_01@mail.ru>
2016-05-28 10:47:49 +0400

    Добавлены решения для 3 и 4 заданий

--- Files/Files/Files/Program.cs
+++ Files/Files/Files/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using Microsoft.VisualBasic.FileIO;    
+using Microsoft.VisualBasic.FileIO;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,10 +39,6 @@ namespace Files
             }
         }
 
-        static void Main(string[] args)
-        {
-        }
-
         public static IEnumerable<T> ReadCSV2<T>(string filename)
         {
             var lines = ReadCSV1(filename);
@@ -48,17 +46,17 @@ namespace Files
                 .First()
                 .Split(',')
                 .Select(x => x.Substring(1, x.Length - 2))
-                .Select(x => x.Replace(".",""))
+                .Select(x => x.Replace(".", ""))
                 .ToArray();
             foreach (var line in lines.Skip(1))
             {
                 var lineValues = line.Split(',');
-                var resultItemConstructor = typeof (T).GetConstructor(new Type[] {});
-                var resultItem = resultItemConstructor.Invoke(new object[] {});
+                var resultItemConstructor = typeof(T).GetConstructor(new Type[] { });
+                var resultItem = resultItemConstructor.Invoke(new object[] { });
                 for (var i = 0; i < fieldNames.Length; i++)
                 {
-                    var type = typeof (T).GetProperty(fieldNames[i]).PropertyType;
-                    if (type == typeof (int))
+                    var type = typeof(T).GetProperty(fieldNames[i]).PropertyType;
+                    if (type == typeof(int))
                     {
                         try
                         {
@@ -70,7 +68,7 @@ namespace Files
                         }
                         continue;
                     }
-                    if (type == typeof (int?))
+                    if (type == typeof(int?))
                     {
                         try
                         {
@@ -82,7 +80,7 @@ namespace Files
                         }
                         continue;
                     }
-                    if (type == typeof (double))
+                    if (type == typeof(double))
                     {
                         try
                         {
@@ -106,11 +104,11 @@ namespace Files
                         }
                         continue;
                     }
-                    if (type == typeof (string))
+                    if (type == typeof(string))
                     {
                         try
                         {
-                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, lineValues[i].Substring(1, lineValues[i].Length-2));
+                            typeof(T).GetProperty(fieldNames[i]).SetValue(resultItem, lineValues[i].Substring(1, lineValues[i].Length - 2));
                         }
                         catch (Exception)
                         {
@@ -120,8 +118,97 @@ namespace Files
                     }
 
                 }
-                yield return (T) resultItem;
+                yield return (T)resultItem;
             }
+        }
+
+        public static IEnumerable<Dictionary<string, object>> ReadCSV3(string filename)
+        {
+            var lines = ReadCSV1(filename);
+            var fieldNames = lines
+                .First()
+                .Split(',')
+                .Select(x => x.Substring(1, x.Length - 2))
+                .Select(x => x.Replace(".", ""))
+                .ToArray();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var lineValues = line.Split(',');
+                var resultItem = new Dictionary<string, object>();
+                for (var i = 0; i < fieldNames.Length; i++)
+                {
+                    if (lineValues[i].Equals("NA"))
+                    {
+                        resultItem[fieldNames[i]] = null;
+                        continue;
+                    }
+
+                    int iValue;
+                    if (int.TryParse(lineValues[i], out iValue))
+                    {
+                        resultItem[fieldNames[i]] = iValue;
+                        continue;
+                    }
+
+                    double dValue;
+                    if (double.TryParse(lineValues[i], out dValue))
+                    {
+                        resultItem[fieldNames[i]] = dValue;
+                        continue;
+                    }
+
+                    resultItem[fieldNames[i]] = lineValues[i].Substring(1, lineValues[i].Length - 2);
+                }
+                yield return resultItem;
+            }
+        }
+
+        public static IEnumerable<dynamic> ReadCSV4(string filename)
+        {
+            var lines = ReadCSV1(filename);
+            var fieldNames = lines
+                .First()
+                .Split(',')
+                .Select(x => x.Substring(1, x.Length - 2))
+                .Select(x => x.Replace(".", ""))
+                .ToArray();
+
+            foreach (var line in lines.Skip(1))
+            {
+                var lineValues = line.Split(',');
+                dynamic resultItem = new ExpandoObject();
+                for (var i = 0; i < fieldNames.Length; i++)
+                {
+                    if (lineValues[i].Equals("NA"))
+                    {
+                        ((IDictionary<string, object>)resultItem)[fieldNames[i]] = null;
+                        continue;
+                    }
+
+                    int iValue;
+                    if (int.TryParse(lineValues[i], out iValue))
+                    {
+                        ((IDictionary<string, object>)resultItem)[fieldNames[i]] = iValue;
+                        continue;
+                    }
+
+                    double dValue;
+                    if (double.TryParse(lineValues[i], NumberStyles.AllowDecimalPoint, new CultureInfo("en-us"), out dValue))
+                    {
+                        ((IDictionary<string, object>)resultItem)[fieldNames[i]] = dValue;
+                        continue;
+                    }
+
+                    ((IDictionary<string, object>)resultItem)[fieldNames[i]] = lineValues[i].Substring(1, lineValues[i].Length - 2);
+                }
+                yield return resultItem;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
         }
     }
 }
--- Files/Files/Files/Tests.cs
+++ Files/Files/Files/Tests.cs
@@ -15,15 +15,34 @@ namespace Files
         }
 
         [Test]
-        public void TestSecondReadCSV2()
+        public void TestSecondReadCSV()
         {
             var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
             var objs = Program.ReadCSV2<AirQualityItem>(filename);
             var sensor1 = objs.Take(1).ToArray()[0];
             Assert.AreEqual("Sensor1",sensor1.Name);
             Assert.AreEqual(41, sensor1.Ozone);
+
+            
         }
 
+        [Test]
+        public void TestThirdReadCSV()
+        {
+            var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
+            var objs = Program.ReadCSV3(filename);
+            var sensor1 = objs.Take(1).ToArray()[0];
+            Assert.AreEqual("Sensor1", (string)sensor1["Name"]);
+            Assert.AreEqual(41, (int)sensor1["Ozone"]);
+        }
 
+        [Test]
+        public void TestFourthReadCSV()
+        {
+            var filename = @"C:\GitHub\Study\Files\Files\Files\bin\Debug\airquality.csv";
+            var windValues = Program.ReadCSV4(filename).Where(z => z.Ozone > 10).Select(z => z.Wind);
+            Assert.AreEqual(7.4, windValues.First());
+            Assert.AreEqual(11.5, windValues.Last());
+        }
     }
 }
